23e3b4823f4db44c21622e61301dc3fbd5f41942
liuchaoxiong <carlosliucx@163.com>
2021-06-17 17:29:05 +0800

    发送邮件

--- service/src/main/java/com/joker/service/impl/EmailServiceImpl.java
+++ service/src/main/java/com/joker/service/impl/EmailServiceImpl.java
@@ -1,13 +1,34 @@
 package com.joker.service.impl;
 
 import com.joker.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
     @Override
     public String send(String address, String content) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(address);
+        message.setSubject(content);
+        message.setText(content);
+
+        mailSender.send(message);
         return "send " + content + " to " + address;
     }
+
+
+
 }
